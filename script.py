29d7ab4ef7e8a6bde2c07421cb8a0491ae8b9336
Cory <coryewest@gmail.com>
2024-07-14 04:51:19 +0000

    Starting my python playground.

---
+++ script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def my_function():
+    """This is a doc string.
+    This can be useful.
+    It's pretty cool, isn't it?
+    """
+    mynum1 = 10
+    mynum2 = 30
+    print(mynum1 + mynum2)
+
+my_function()
+my_function()
+
